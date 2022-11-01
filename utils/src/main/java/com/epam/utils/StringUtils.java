a550d93417a25e93335a2412418c3d3a4a5a5620
ShulpinP <96373490+ShulpinP@users.noreply.github.com>
2022-11-01 13:27:24 +0300

    4

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,8 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (Integer.valueOf(str) > 0) {
-            return true;
-        } return false;
+        return (Double.parseDouble(str) > 0) && Double.valueOf(str).isNaN();
     }
 }
